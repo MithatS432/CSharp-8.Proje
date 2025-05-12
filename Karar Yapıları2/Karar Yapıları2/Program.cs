67f9b9aeaaaacd7c550577db12994571a8b1230c
Mithat <mithatsirmali@gmail.com>
2025-05-12 15:59:03 +0300

    Ders 8
    Ders 8 Eklenmiştir..

---
+++ Karar Yapıları2/Karar Yapıları2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karar_Yapıları2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen haftanın kaçıncı günü olduğunu girin (1-7): ");
+            int gun = int.Parse(Console.ReadLine());
+
+            switch (gun)
+            {
+                case 1:
+                    Console.WriteLine("Bugün: Pazartesi");
+                    break;
+                case 2:
+                    Console.WriteLine("Bugün: Salı");
+                    break;
+                case 3:
+                    Console.WriteLine("Bugün: Çarşamba");
+                    break;
+                case 4:
+                    Console.WriteLine("Bugün: Perşembe");
+                    break;
+                case 5:
+                    Console.WriteLine("Bugün: Cuma");
+                    break;
+                case 6:
+                    Console.WriteLine("Bugün: Cumartesi");
+                    break;
+                case 7:
+                    Console.WriteLine("Bugün: Pazar");
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz gün sayısı!");
+                    break;
+            }
+        }
+    }
+}
